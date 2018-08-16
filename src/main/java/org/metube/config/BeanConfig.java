77c18598d0d8294fcd3bd5550ee65ea7089c5fbd
mario <axereliat@gmail.com>
2018-08-16 13:34:29 +0300

    Another commit...

--- src/main/java/org/metube/config/BeanConfig.java
+++ src/main/java/org/metube/config/BeanConfig.java
@@ -16,6 +16,9 @@ public class BeanConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.mailtrap.io");
+        mailSender.setUsername("0268e3c634130b");
+        mailSender.setPassword("cc146fde29a5a6");
 
         return mailSender;
     }
